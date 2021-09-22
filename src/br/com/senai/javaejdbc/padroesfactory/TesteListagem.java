6d326a7578d46c56154b54f8f061ac7d4096539c
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-22 11:21:13 -0300

    Listando a tabela PRODUTO

---
+++ src/br/com/senai/javaejdbc/padroesfactory/TesteListagem.java
@@ -0,0 +1,29 @@
+package br.com.senai.javaejdbc.padroesfactory;
+
+import java.sql.*;
+import java.util.List;
+
+public class TesteListagem {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager
+                .getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
+
+        Statement stm = connection.createStatement();
+        stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+
+        ResultSet rst = stm.getResultSet();
+
+        while (rst.next()) {
+            Integer id = rst.getInt("ID");
+            System.out.println(id);
+            String nome = rst.getString("NOME");
+            System.out.println(nome);
+            String descricao = rst.getString("DESCRICAO");
+            System.out.println(descricao);
+
+        }
+
+        connection.close();
+    }
+}
