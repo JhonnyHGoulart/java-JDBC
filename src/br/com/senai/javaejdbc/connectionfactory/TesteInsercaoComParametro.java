4a1939e72ae9b26cc15f8a963689b73f50a69c74
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-23 09:57:06 -0300

    Inserindo dados com o método prepareStatement()

---
+++ src/br/com/senai/javaejdbc/connectionfactory/TesteInsercaoComParametro.java
@@ -0,0 +1,25 @@
+package br.com.senai.javaejdbc.connectionfactory;
+
+import java.sql.*;
+
+public class TesteInsercaoComParametro {
+    public static void main(String[] args) throws SQLException {
+        String nome = "Mouse";
+        String descricao = "Mouse sem fio";
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.recuperarConexao();
+
+        PreparedStatement stm =
+                connection.prepareStatement("INSERT INTO PRODUTO (nome,descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+        stm.setString(1, nome);
+        stm.setString(2, descricao);
+        stm.execute();
+
+        ResultSet rst = stm.getGeneratedKeys();
+        while (rst.next()) {
+            Integer id = rst.getInt(1);
+            System.out.println("O id criado foi: " + id);
+        }
+    }
+}
