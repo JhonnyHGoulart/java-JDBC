9e1a88efe984b97680ecc3bcf94b1b06fb36a061
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-24 11:47:06 -0300

    Criando um pool de conexões com a classe ComboPooledDatasource

--- src/br/com/senai/javaejdbc/connectionfactory/ConnectionFactory.java
+++ src/br/com/senai/javaejdbc/connectionfactory/ConnectionFactory.java
@@ -1,14 +1,26 @@
 package br.com.senai.javaejdbc.connectionfactory;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+        public DataSource dataSource;
+
+    public ConnectionFactory() {
+            ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+            comboPooledDataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+            comboPooledDataSource.setUser("postgres");
+            comboPooledDataSource.setPassword("postgres");
+
+            this.dataSource = comboPooledDataSource;
+    }
 
     public Connection recuperarConexao() throws SQLException {
-        return DriverManager
-                .getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
+        return this.dataSource.getConnection();
 
+//        return DriverManager
+//                .getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
     }
 }
