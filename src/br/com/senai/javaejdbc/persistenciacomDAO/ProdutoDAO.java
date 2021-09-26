aafb4bab8a8d9e391f07033de51229019c80fec1
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-26 13:53:20 -0300

    Listando produto com ProdutoDAO

--- src/br/com/senai/javaejdbc/persistenciacomDAO/ProdutoDAO.java
+++ src/br/com/senai/javaejdbc/persistenciacomDAO/ProdutoDAO.java
@@ -3,6 +3,8 @@ package br.com.senai.javaejdbc.persistenciacomDAO;
 import br.com.senai.javaejdbc.connectionfactory.ConnectionFactory;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutoDAO {
     private Connection connection;
@@ -12,19 +14,38 @@ public class ProdutoDAO {
     }
 
     public void salvar(Produto produto) throws SQLException {
-        try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-            String sql = "INSERT INTO PRODUTO (ID,NOME,DESCRICAO) VALUES (?,?,?)";
-            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-                pstm.setInt(1, 3);
-                pstm.setString(2, produto.getNome());
-                pstm.setString(3, produto.getDescricao());
+        String sql = "INSERT INTO PRODUTO (ID,NOME,DESCRICAO) VALUES (?,?,?)";
+        try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstm.setInt(1, produto.getId());
+            pstm.setString(2, produto.getNome());
+            pstm.setString(3, produto.getDescricao());
+            pstm.execute();
 
-                try (ResultSet rst = pstm.getGeneratedKeys()) {
-                    while (rst.next()) {
-                        produto.setId((rst.getInt(1)));
-                    }
+            try (ResultSet rst = pstm.getGeneratedKeys()) {
+                while (rst.next()) {
+                    produto.setId((rst.getInt(1)));
                 }
             }
         }
     }
+
+    public List<Produto> listarProdutos() throws SQLException {
+        List<Produto> produtos = new ArrayList<Produto>();
+        String sql = "SELECT * FROM PRODUTO";
+        try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.execute();
+
+            try (ResultSet rst = pstm.getResultSet()) {
+                while (rst.next()) {
+                    Produto produto =
+                            new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
+
+                    produtos.add(produto);
+                }
+            }
+        }
+
+        return produtos;
+    }
+
 }
--- src/br/com/senai/javaejdbc/persistenciacomDAO/TesteInsercaoElistagemComProduto.java
+++ src/br/com/senai/javaejdbc/persistenciacomDAO/TesteInsercaoElistagemComProduto.java
@@ -4,14 +4,19 @@ import br.com.senai.javaejdbc.connectionfactory.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
-public class TesteInsercaoComProduto2 {
+public class TesteInsercaoElistagemComProduto {
     public static void main(String[] args) throws SQLException {
 
         Produto comoda = new Produto(3, "Cômoda", "Cômoda Vertical");
         try (Connection connection = new ConnectionFactory().recuperarConexao()) {
             ProdutoDAO produtoDAO = new ProdutoDAO(connection);
             produtoDAO.salvar(comoda);
+
+            List<Produto> listaDeProdutos = produtoDAO.listarProdutos();
+            listaDeProdutos.stream()
+                    .forEach(lp -> System.out.println(lp));
         }
     }
 }
